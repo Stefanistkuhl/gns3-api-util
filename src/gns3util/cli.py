7fe03fbaa99cc6a1e7066e85cb0cfc26a9b4f9da
Stefanistkuhl <ghgstefan@gmail.com>
2025-03-15 15:48:52 +0100

    feat: bound `get` endpoints to commands

--- src/gns3util/cli.py
+++ src/gns3util/cli.py
@@ -1,14 +1,14 @@
 import click
 from .auth import auth
-
+from .get import get
 
 @click.group()
 def gns3util():
     """A utility for GNS3."""
     pass
 
-
 gns3util.add_command(auth)
+gns3util.add_command(get)
 
 if __name__ == '__main__':
     gns3util()
