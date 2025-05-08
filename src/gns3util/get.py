a2d16d86608993c096cf5ff8229fc514e3b89a71
Eduard <daneduardleuska@gmail.com>
2025-05-08 14:41:22 +0200

    refactor: added help and epilog as descriptions in get.py

--- src/gns3util/get.py
+++ src/gns3util/get.py
@@ -81,20 +81,20 @@ with open(help_path, "r") as f:
 
 # Create click commands with zero arguments
 for cmd, func in _zero_arg.items():
-    current_help_option = get_command_description(cmd, help_dict, "zero_arg")
-    def make_cmd(func=func, help_option=current_help_option):
+    current_help_option,epiloge = get_command_description(cmd, help_dict, "zero_arg")
+    def make_cmd(func=func, help_option=current_help_option,epilog=epiloge):
         @click.pass_context
         def cmd_func(ctx):
             api_get_client = get_client(ctx)
             execute_and_print(
                 ctx, api_get_client, lambda client: getattr(api_get_client, func)())
         return cmd_func
-    get.command(name=cmd, help=current_help_option)(make_cmd())
+    get.command(name=cmd, help=current_help_option,epilog=epiloge)(make_cmd())
 
 # Create click commands with one argument
 for cmd, func in _one_arg.items():
-    current_help_option = get_command_description(cmd, help_dict, "one_arg")
-    def make_cmd(func=func, help_option=current_help_option):
+    current_help_option,epiloge = get_command_description(cmd, help_dict, "one_arg")
+    def make_cmd(func=func, help_option=current_help_option,epilog=epiloge):
         @click.argument('arg')
         @click.pass_context
         def cmd_func(ctx, arg):
@@ -102,12 +102,12 @@ for cmd, func in _one_arg.items():
             execute_and_print(
                 ctx, api_get_client, lambda client: getattr(api_get_client, func)(arg))
         return cmd_func
-    get.command(name=cmd, help=current_help_option)(make_cmd())
+    get.command(name=cmd, help=current_help_option,epilog=epiloge)(make_cmd())
 
 # Create click commands with two arguments
 for cmd, func in _two_arg.items():
-    current_help_option = get_command_description(cmd, help_dict, "two_arg")
-    def make_cmd(func=func, help_option=current_help_option):
+    current_help_option,epiloge = get_command_description(cmd, help_dict, "two_arg")
+    def make_cmd(func=func, help_option=current_help_option,epilog=epiloge):
         @click.argument('project_id')
         @click.argument('id')
         @click.pass_context
@@ -116,7 +116,7 @@ for cmd, func in _two_arg.items():
             execute_and_print(ctx, api_get_client, lambda client: getattr(
                 api_get_client, func)(project_id, id))
         return cmd_func
-    get.command(name=cmd, help=current_help_option)(make_cmd())
+    get.command(name=cmd, help=current_help_option,epilog=epiloge)(make_cmd())
 
 # Special commands with timeout options
 
