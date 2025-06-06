f574d024bee69497177580ae109754bbbda6347a
PatrickMagAnime <riedl.patrick1707@gmail.com>
2025-06-06 15:32:11 +0200

    adding wva feature

--- src/gns3util/fuzzy.py
+++ src/gns3util/fuzzy.py
@@ -2,7 +2,7 @@ import click
 import os
 from . import auth
 from .api.get_endpoints import GNS3GetAPI
-from .utils import fuzzy_info_wrapper, get_fuzzy_info_params, fuzzy_params_type, fuzzy_put_wrapper, fuzzy_password_params
+from .utils import fuzzy_info_wrapper, get_fuzzy_info_params, fuzzy_params_type, fuzzy_put_wrapper, fuzzy_password_params, activate_wva_device
 
 
 def get_client(ctx):
@@ -140,3 +140,32 @@ def change_password(ctx, multi):
         multi=multi,
     )
     fuzzy_put_wrapper(params)
+
+
+@fuzzy.command(name="wva", help="Activate a bluetooth-controlled device for a user.")
+@click.option(
+    "-u", "--username", required=True, help="The user whose device should be activated."
+)
+@click.option(
+    "--activate/--no-activate", default=False, help="Activate bluetooth device."
+)
+@click.option(
+    "--strength", type=int, default=1, show_default=True, help="Strength for the bluetooth device (e.g. 1-10)."
+)
+@click.pass_context
+def activate_wva(ctx, username, activate, strength):
+    """
+    Activates a bluetooth-controlled device for the user if --activate is set.
+    The strength can be set using --strength.
+    """
+    if activate:
+        click.secho(f"Activating bluetooth device for user {
+                    username} with strength {strength} ...", fg="cyan")
+        result = activate_wva_device(username, strength)
+        if result:
+            click.secho("Bluetooth device activated.", fg="green")
+        else:
+            click.secho("Activation failed.", fg="red")
+    else:
+        click.secho(
+            "Bluetooth device was not activated (missing --activate flag).", fg="yellow")
